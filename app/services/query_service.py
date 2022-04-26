d660896e5c84b8aad750b190fb8ed8276a97b3a3
Bruno Tetzner <brunoltetzner@gmail.com>
2022-04-26 14:21:39 -0300

    feat: created get_all_svc function to get the informations by models

--- app/services/query_service.py
+++ app/services/query_service.py
@@ -1,4 +1,8 @@
-def get_all_svc(model):...
+def get_all_svc(Model, order=None):
+    all_things = Model.query.order_by(order.desc()).all() if order != None else Model.query.all()
+    if len(all_things) == 0:
+         raise...
+    return all_things
 
 def get_by_id_svc(model, id):...
 
