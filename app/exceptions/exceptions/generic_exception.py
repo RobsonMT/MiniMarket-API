ce22d3674063630c123cd61bb3c98adba4a1788d
Gabriel Amaral <oamaral@pop-os.localdomain>
2022-04-26 12:01:10 -0300

    feat:created generic exceptions and specific

---
+++ app/exceptions/exceptions/generic_exception.py
@@ -0,0 +1,29 @@
+from http import HTTPStatus
+
+class ErrorFound(Exception):
+    def __init__(self, message=None, status_code=HTTPStatus.CONFLICT):
+
+        if not message:
+            self.message = "Error in values"
+        else:
+            self.message = message
+
+        self.status_code = status_code
+
+class IdNotFound(Exception):...
+    
+class ErrorFilter(Exception):...
+
+class TableEmpty(Exception):...
+
+class TableNotFound(Exception):...
+
+class AttributeTypeError(Exception):...
+
+class AttributeValueError(Exception):...
+
+class WrongKeyError(Exception):...
+
+class MissingKeyError(Exception):...
+
+class UnauthorizedUser(Exception):...
