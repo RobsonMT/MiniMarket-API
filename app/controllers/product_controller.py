f81d27e76bae5047bbdcbb787cba20780329e6a7
Rhamon Souza <rhamo1@outlook.com>
2022-05-02 18:56:59 -0300

    feat: patch product function.

--- app/controllers/product_controller.py
+++ app/controllers/product_controller.py
@@ -1,16 +1,20 @@
 from http import HTTPStatus
+from itertools import product
+from turtle import update
 
-from flask import jsonify, request
-from flask_jwt_extended import jwt_required
+from flask import jsonify, request, session
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from psycopg2.errors import UniqueViolation
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm.session import Session
 
 from app.configs.database import db
+from app.exceptions.generic_exception import IdNotFound, UnauthorizedUser
 from app.models import ProductModel
 from app.models.categories_model import CategoryModel
+from app.models.establishment_model import EstablishmentModel
 from app.models.product_categories import ProductCategory
-from app.services.query_service import create_svc
+from app.services.query_service import create_svc, filter_svc, get_by_id_svc, update_svc
 
 
 def create_one_product() -> dict:
@@ -53,9 +57,30 @@ def get_product_by_id(establishment_id: int, product_id: int) -> dict:
 
 
 @jwt_required()
-def patch_product(id: int) -> dict:
-    """
-    rota protegida: verifica se o dono da aplicação tem o producte com base no id
-    arquivar producte
-    """
-    return "Rota patch product"
+def patch_product(establishment_id: int, product_id: int) -> dict:
+    data = request.get_json()
+    session: Session = db.session
+    query = session.query(ProductModel)
+
+    try:
+        products = jsonify(query.filter_by(establieshment_id=establishment_id, id=product_id).first())
+        val_prod = products.get_json()
+
+        if val_prod:
+            for key, value in val_prod.items():
+                if key=='establieshment_id':
+                    establishment_product_id = value
+        else:
+            raise IdNotFound
+
+        if establishment_product_id == establishment_id:
+            update = update_svc(ProductModel, product_id, data)
+
+            return jsonify(update), 200
+
+    except IdNotFound:
+        return jsonify(error = "Product ID doesn't exists."), 400
+    except IntegrityError:
+        return jsonify(error = "Product ID already exists."), 400
+
+
