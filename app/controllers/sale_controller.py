ae7cea3da06e1d31441a0fcfcc85bc427c6ea140
elbulidur <juliocezar.marketing@gmail.com>
2022-05-04 23:41:41 -0300

    feat: create route post

--- app/controllers/sale_controller.py
+++ app/controllers/sale_controller.py
@@ -1,18 +1,48 @@
+
 from http import HTTPStatus
+from pydoc import cli
 
 from flask import jsonify, request
 from flask_jwt_extended import get_jwt_identity, jwt_required
 from sqlalchemy.exc import IntegrityError
 
 from app.exceptions import FilterError, IdNotFound, UnauthorizedUser
-from app.models import AddressModel, SaleModel
+from app.models import AddressModel, SaleModel, SaleProductModel, ClientModel, PaymentModel
 from app.services import query_service
+from functools import reduce
 
 
 @jwt_required()
 def post_sale():
     data = request.get_json()
+    
     try:
+        products = data.pop("products")
+        data['sale_total'] = reduce(lambda x, y: x + y, [product['sale_price'] for product in products]) #sale_amount
+        
+        sale = query_service.create_svc(SaleModel, data)
+        
+        for product in products:
+            data = {"product_id": product.id, "sale_id": sale.id}
+            query_service.create_svc(SaleProductModel, data)
+            
+        new_data = {
+            "id":sale.id,
+            "date":sale.date,
+            "paid_date": sale.paid_date,
+            "client": query_service.filter_svc(ClientModel, {"id":sale.client_id}).name,
+            "payment": query_service.filter_svc(PaymentModel, {"id":sale.payment_id}).form_of_payment,
+            "sale_total": sale.sale_total,
+            "remain_to_pay": sale.remain_to_pay
+        }
+        
+        return new_data, 201
+    
+    except UnauthorizedUser:
+        return {"error": "Unauthorized user."}, 401
+    except IntegrityError:
+        return {"error": "Sale (ID) already exists."}, 409
+        
         query_service.create_svc(SaleModel, data)
         return jsonify(data), 201
     except UnauthorizedUser:
