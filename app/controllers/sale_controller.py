1c2de7e5f0944b53eaf93c3e92792255becced72
elbulidur <juliocezar.marketing@gmail.com>
2022-05-05 13:16:02 -0300

    fix: update in route patch_sale

--- app/controllers/sale_controller.py
+++ app/controllers/sale_controller.py
@@ -1,66 +1,33 @@
-
 from http import HTTPStatus
-from pydoc import cli
 
 from flask import jsonify, request
 from flask_jwt_extended import get_jwt_identity, jwt_required
 from sqlalchemy.exc import IntegrityError
 
-from app.exceptions import FilterError, IdNotFound, UnauthorizedUser
-from app.models import AddressModel, SaleModel, SaleProductModel, ClientModel, PaymentModel
+from app.exceptions import UnauthorizedUser, FilterError, IdNotFound
+from app.models import SaleModel, AddressModel
 from app.services import query_service
-from functools import reduce
 
 
 @jwt_required()
 def post_sale():
     data = request.get_json()
-    
     try:
-        products = data.pop("products")
-        data['sale_total'] = reduce(lambda x, y: x + y, [product['sale_price'] for product in products]) #sale_amount
-        
-        sale = query_service.create_svc(SaleModel, data)
-        
-        for product in products:
-            data = {"product_id": product.id, "sale_id": sale.id}
-            query_service.create_svc(SaleProductModel, data)
-            
-        new_data = {
-            "id":sale.id,
-            "date":sale.date,
-            "paid_date": sale.paid_date,
-            "client": query_service.filter_svc(ClientModel, {"id":sale.client_id}).name,
-            "payment": query_service.filter_svc(PaymentModel, {"id":sale.payment_id}).form_of_payment,
-            "sale_total": sale.sale_total,
-            "remain_to_pay": sale.remain_to_pay
-        }
-        
-        return new_data, 201
-    
-    except UnauthorizedUser:
-        return {"error": "Unauthorized user."}, 401
-    except IntegrityError:
-        return {"error": "Sale (ID) already exists."}, 409
-        
         query_service.create_svc(SaleModel, data)
         return jsonify(data), 201
     except UnauthorizedUser:
         return {"error": "Unauthorized user."}, 401
-        
-    # except IntegrityError:
-    #     return {"error": "Sale (ID) already exists."}, 409
-
+    except IntegrityError:
+        return {"error": "Sale (ID) already exists."}, 409
 
 @jwt_required()
 def patch_sale(id):
-    data = request.get_json()
-
+    data= request.get_json()
+    
     try:
         return query_service.update_svc(SaleModel, id, data)
     except IdNotFound as err:
-        return err.args[0], err.args[1]
-
+        return {"error": "key doesn't exist"}, HTTPStatus.BAD_REQUEST
 
 @jwt_required()
 def get_sales(client_id):
